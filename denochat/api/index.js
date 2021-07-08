5a86e574227c5d770fbeb9599cc62a122c3b558f
Maximiliano Usich <maximilianou@gmail.com>
2021-07-08 18:47:04 -0300

    feat(api): add deno module

--- denochat/api/index.js
+++ denochat/api/index.js
@@ -1,7 +1,17 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("Hi Deno Chat Step One!!", {
-    status: 200, 
-    headers: { "content-type": "text/plain"}
-  });
-  event.respondWith(response);
+import { Application } from 'https://deno.lang/x/oak/mod.ts';
+
+const app  = new Application();
+
+app.use( (ctx) => {
+  ctx.response.body = 'Hi, from oak deno module!!';
 });
+
+addEventListener('fetch', app.fetchEventHandler());
+
+//addEventListener("fetch", (event) => {
+//  const response = new Response("Hi Deno Chat Step One!!", {
+//    status: 200, 
+//    headers: { "content-type": "text/plain"}
+//  });
+//  event.respondWith(response);
+//});
